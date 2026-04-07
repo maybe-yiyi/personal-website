ef6e1a192280b04347a0c5ab2c417f13f4e49ae9
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-07 00:46:47 -0400

    feat: made default background color black
    so i don't get flashbanged

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id="main">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
+
+<style>
+    :global(body) {
+        background-color: black;
+    }
+
+    #main {
+        color: white;
+    }
+</style>
