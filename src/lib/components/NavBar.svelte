a09eeead140ad94b73cc5db5398aca8c88ff0b86
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-07 01:46:09 -0400

    feat: basic nav bar and hero section

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,15 @@
+<nav>
+    <div id="home">yy.liu</div>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+    }
+
+    #home {
+        margin: 20px 30px;
+        color: #01A4FF;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,58 @@
+<script>
+    import { NavBar } from '$lib';
+</script>
+
 <div id="main">
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <NavBar />
+    <section id="hero">
+        <div id="hero-content">
+            <p id="hello">Hello I'm</p>
+            <h1>Yiyoung<br />Liu</h1>
+            <p>Information Systems @ CMU<br />Class of 2029</p>
+        </div>
+    </section>
 </div>
 
 <style>
     :global(body) {
+        margin: 0;
+        padding: 0;
+
         background-color: black;
         font-family: 'Geist Mono', monospace;
     }
 
     #main {
         color: white;
+    }
+
+    /* hero stuff */
+    #hero {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        padding: 20px 8px;
+    }
+
+    #hero-content {
+        max-width: 48rem;
+        width: 100%;
+        margin-inline: auto;
+    }
+
+    #hello {
+        font-size: 0.75rem;
+        text-transform: uppercase;
+        color: #01A4FF;
+        margin-botton: 1.5rem;
+    }
+
+    #hero-content h1 {
+        font-family: sans-serif;
+        font-size: 8rem;
+        font-weight: bold;
+
+        margin-top: 0rem;
+        margin-bottom: 1.5rem;
     }
 </style>
